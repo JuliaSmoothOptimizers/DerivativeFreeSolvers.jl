9cbb1ba35dde2a87cc2677791139cb2b7cf8a64a
Abel Soares Siqueira <abel.s.siqueira@gmail.com>
2019-08-11 11:19:52 -0300

    Implement MADS

---
+++ src/mads.jl
@@ -0,0 +1,146 @@
+using Random
+
+export mads
+
+"""
+    mads(nlp)
+
+MADS is implemented following the description of
+
+    Mark A. Abramson, Charles Audet, J. E. Dennis Jr., and Sébastien Le Digabel.
+    OrthoMADS: A Deterministic MADS Instance with Orthogonal Directions.
+    SIAM Journal on Optimization, 2009, Vol. 20, No. 2, pp. 948-966,
+
+But using random vectors instead of Halton vectors, as suggested by Francisco Sobral.
+"""
+function mads(nlp :: AbstractNLPModel;
+              x :: AbstractVector=copy(nlp.meta.x0),
+              atol :: Real=√eps(eltype(x)),
+              rtol :: Real=√eps(eltype(x)),
+              max_eval :: Int=nlp.meta.nvar*200,
+              max_time :: Float64=30.0,
+              max_iter :: Int=-1,
+              stop_with_small_step :: Bool=false,
+              steptol :: Real=1e-8,
+              greedy :: Bool=false,
+             )
+
+  T = eltype(x)
+  f(x) = obj(nlp, x)
+
+  # TODO: Use a filter
+  P(x) = if unconstrained(nlp)
+    zero(eltype(x))
+  else
+    bl, bu, cl, cu = [getfield(nlp.meta, f) for f in [:lvar,:uvar,:lcon,:ucon]]
+    p = sum(max(zero(T), x[i] - bu[i], bl[i] - x[i])^2 for i = 1:nlp.meta.nvar)
+    if nlp.meta.ncon > 0
+      cx = cons(nlp, x)
+      p += sum(max(zero(T), cx[i] - cu[i], cl[i] - cx[i])^2 for i = 1:nlp.meta.ncon)
+    end
+    p
+  end
+
+  n = nlp.meta.nvar
+
+  fx = f(x)
+  Px = P(x)
+  ftol = atol + abs(fx) * rtol
+
+  Δ = min(one(T), minimum(nlp.meta.uvar - nlp.meta.lvar) / 10)
+  μ = one(T)
+  ϕx = fx + μ * Px
+  Δmin = eps(T)
+
+  iter = 0
+  start_time = time()
+  elapsed_time = 0.0
+  tired = neval_obj(nlp) ≥ max_eval ≥ 0 || elapsed_time > max_time || iter > max_iter ≥ 0
+  ∂f = Inf
+  satisfied = stop_with_small_step && Δ ≤ steptol
+
+  seq = 0
+
+  xt = similar(x)
+
+  q = randn(n)
+  # Dk = [Hk -Hk]
+  # Hk = qᵀq I - 2 qqᵀ
+  # Hk eⱼ = qᵀq eⱼ - 2 qᵀeⱼ q = qᵀq eⱼ - 2qⱼ q
+
+  # TODO: Use SolverTools log
+  @info log_header([:iter, :nf, :f, :P, :Δ, :μ, :status], [Int, Int, T, T, T, T, String],
+                   hdr_override=Dict(:f=>"f(x)", :P=>"P(x)"))
+
+  @info log_row(Any[iter, neval_obj(nlp), fx, Px, Δ, μ])
+
+  while !(satisfied || tired)
+    decrease = false
+    status = "No decrease"
+    besti = 0
+    bestf, bestP, bestϕ = fx, Px, ϕx
+    #∂f = 0.0
+    for s = [1, -1], i = 1:n
+      xt .= x .- (2s * Δ * q[i]) .* q
+      xt[i] += dot(q, q) * s * Δ
+      ft = f(xt)
+      Pt = P(xt)
+      ϕt = ft + μ * Pt
+      #∂f = max(∂f, abs(ft - fx) / Δ)
+      if ϕt < bestϕ
+        decrease = true
+        status = "Decrease at i = $i, s = $s"
+        besti = s * i
+        bestf, bestP, bestϕ = ft, Pt, ϕt
+        greedy || break
+      end
+    end
+    if decrease
+      fx, Px = bestf, bestP
+      i = abs(besti)
+      s = sign(besti)
+      x .-= (2s * Δ * q[i]) .* q
+      x[i] += dot(q, q) * s * Δ
+      seq = max(seq + 1, 1)
+      Δ *= T(4)^seq
+    else
+      seq = min(seq - 1, -1)
+      Δ /= T(2)^(-1 / seq)
+      if Px > 0
+        μ *= 2
+      end
+    end
+    iter += 1
+    if !stop_with_small_step
+      Δ = max(Δmin, Δ)
+    end
+
+    @info log_row(Any[iter, neval_obj(nlp), fx, Px, Δ, μ, status])
+
+    randn!(q)
+    ϕx = fx + μ * Px
+
+    satisfied = (stop_with_small_step && Δ ≤ steptol) || (!decrease && ∂f ≤ ftol)
+    elapsed_time = time() - start_time
+    tired = neval_obj(nlp) ≥ max_eval ≥ 0 || elapsed_time > max_time || iter > max_iter ≥ 0
+  end
+
+  status = if Δ ≤ steptol
+    :small_step
+  elseif ∂f ≤ ftol
+    :stalled
+  elseif tired
+    if neval_obj(nlp) ≥ max_eval ≥ 0
+      :max_eval
+    elseif elapsed_time > max_time
+      :max_time
+    else
+      :max_iter
+    end
+  else
+    :unknown
+  end
+
+  return GenericExecutionStats(status, nlp, solution=x, objective=fx, primal_feas=Px,
+                               iter=iter, elapsed_time=elapsed_time)
+end
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,3 +1,81 @@
 using DerivativeFreeSolvers
 
+# JSO
+using NLPModels
 
+# stdlib
+using LinearAlgebra, Logging, Test
+
+function tests()
+  methods = [mads]
+
+  @testset "Every method solves basic problems in multiple precisions" begin
+    for mtd in methods
+      for (f, x₀, x) in [(x->sum(x.^2), [2.1; 3.2], zeros(2)),
+                         (x->(x[1] - 1)^2 + 4 * (x[2] - x[1]^2)^2, zeros(2), ones(2)),
+                         (x->x[1]^2 + x[2]^2 + (x[1] + x[2] - 1)^2, zeros(2), ones(2) / 3),
+                         (x->(x[1] - eltype(x)(1e4))^2 + (x[2] - eltype(x)(2e-4))^2 + (x[1] * x[2] - 2)^2, ones(2), [1e4; 2e-4])
+                        ]
+
+        nlp = ADNLPModel(f, x₀)
+
+        for T in (Float16, Float32, Float64, BigFloat)
+          @testset "Method $mtd in $T" begin
+            reset!(nlp)
+            output = with_logger(NullLogger()) do
+              mtd(nlp, x=T.(x₀), max_eval=10000)
+            end
+            @test norm(output.solution - x) < max(abs(f(x₀)), 1.0) * max(1e-2, sqrt(eps(T)))
+            @test eltype(output.solution) == T
+            @test typeof(output.objective) == T
+          end
+        end
+      end
+    end
+  end
+
+  methods = [mads]
+  @testset "Constrained problems with non-empty interior" begin
+    for mtd in methods
+      for (nlp, sol) in [(ADNLPModel(x->sum(x.^2), [2.1; 3.2], lvar=-ones(2), uvar=4*ones(2)), zeros(2)),
+                         (ADNLPModel(x->sum(x.^2), [2.1; 3.2], lvar=[0.5; 0.5], uvar=4*ones(2)), [0.5; 0.5]),
+                         (ADNLPModel(x->sum(x.^2), [2.1; 3.2], lvar=[0.5; -0.5], uvar=4*ones(2)), [0.5; 0.0]),
+                         (ADNLPModel(x->sum(x.^2), [2.1; 3.2], c=x->[2x[1] + x[2]], lcon=[1.0], ucon=[Inf]), [0.4; 0.2]),
+                         (ADNLPModel(x->sum(x.^2), [-0.1; 0.7], c=x->[x[1]^2 + 2x[2]^2], lcon=[1.0], ucon=[Inf], lvar=zeros(2)), [0.0; sqrt(2)/2]),
+                        ]
+
+        for T in (Float16, Float32, Float64, BigFloat)
+          @testset "Method $mtd in $T" begin
+            reset!(nlp)
+            output = with_logger(NullLogger()) do
+              mtd(nlp, x=T.(nlp.meta.x0), max_eval=10000)
+            end
+            @test norm(output.solution - sol) < max(1e-1, sqrt(eps(T)))
+            @test output.primal_feas < max(1e-1, sqrt(eps(T)))
+            @test eltype(output.solution) == T
+            @test typeof(output.objective) == T
+          end
+        end
+      end
+    end
+  end
+
+  @testset "Testing input" begin
+    for mtd in methods
+      with_logger(NullLogger()) do
+        nlp = ADNLPModel(x -> x[1] + x[2], ones(2))
+        output = mtd(nlp, max_eval=1)
+        @test output.status == :max_eval
+        output = mtd(nlp, max_iter=1)
+        @test output.status == :max_iter
+        output = mtd(nlp, max_time=0.0)
+        @test output.status == :max_time
+
+        nlp = ADNLPModel(x -> x[1]^2 + x[2]^2, ones(2))
+        output = mtd(nlp, atol=1e-2, rtol=1e-2)
+      end
+    end
+  end
+end
+
+tests()
